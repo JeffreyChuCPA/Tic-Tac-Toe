a2f979934d07825a030601a43c38c3085b44b9da
Jeffrey Chu <jeffcolinchu@gmail.com>
2024-06-03 18:24:31 -0400

    Worked on CSS and ResultsCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Board from './components/Board'
 import './App.css'
 import ResultsCard from './components/ResultsCard'
@@ -13,10 +13,15 @@ function App() {
   const [winner, setWinner] = useState<string>('')
   const [currentPlayer, setCurrentPlayer] = useState<string>('')
   const [gameBoard, setGameBoard] = useState<(string | null)[]>(Array(9).fill(null))
+  const [allMarked, setAllMarked] = useState<boolean>(false)
+
+  useEffect(() => {
+    gameBoard.every((cell) => cell !== null) ? setAllMarked(true) : setAllMarked(false)
+  })
 
   const determineFirstTurn = (numberOfPlayers: number): void => {
-    const randomTurn = Math.floor(Math.random() * numberOfPlayers)
-    const firstTurn = randomTurn ? 'p1' : 'p2'
+    const randomTurn: number = Math.floor(Math.random() * numberOfPlayers)
+    const firstTurn: string = randomTurn ? 'p1' : 'p2'
     return setCurrentPlayer(firstTurn)
   }
 
@@ -25,11 +30,12 @@ function App() {
       {!currentPlayer ? determineFirstTurn(2) : null}
       {console.log(currentPlayer)}
       {console.log(gameBoard)}
-      {/* <PlayerCard/> */}
-      <Board gameBoard={gameBoard} setGameBoard={setGameBoard} winner={winner} setWinner={setWinner} currentPlayer={currentPlayer} setCurrentPlayer={setCurrentPlayer}/>
-      {/* <PlayerCard/> */}
-      {winner && console.log(`The Winner is ${winner}`)}
-      <ResultsCard winner={winner} setWinner={setWinner} setGameBoard={setGameBoard} />
+      <div className='app'>
+        {/* <PlayerCard/> */}
+        <Board gameBoard={gameBoard} setGameBoard={setGameBoard} winner={winner} setWinner={setWinner} currentPlayer={currentPlayer} setCurrentPlayer={setCurrentPlayer}/>
+        {/* <PlayerCard/> */}
+        <ResultsCard winner={winner} setWinner={setWinner} allMarked={allMarked} setGameBoard={setGameBoard} />
+      </div>
     </>
   )
 }
--- src/components/ResultsCard.tsx
+++ src/components/ResultsCard.tsx
@@ -3,10 +3,11 @@ import React from "react";
 type ResultsCardProps = {
   winner: string,
   setWinner: React.Dispatch<React.SetStateAction<string>>,
+  allMarked: boolean,
   setGameBoard: React.Dispatch<React.SetStateAction<(string | null)[]>>
 };
 
-const ResultsCard: React.FC<ResultsCardProps> = ({ winner, setWinner, setGameBoard }) => {
+const ResultsCard: React.FC<ResultsCardProps> = ({ winner, setWinner, allMarked, setGameBoard }) => {
 
   const handlePlayAgain = (): void => {
     setWinner('');
@@ -18,11 +19,18 @@ const ResultsCard: React.FC<ResultsCardProps> = ({ winner, setWinner, setGameBoa
     <>
       {winner && 
         <div className="results">
-          {`Winner is ${winner}!`}
+          <span className="results-text">{`Winner is ${winner}!`}</span>
           <button className="results-btn" onClick={() => handlePlayAgain()}>
             Play Again
           </button>
-          </div>}
+        </div>}
+      {!winner && allMarked ? 
+        <div className="results">
+          <span className="results-text">{`It's a tie!`}</span>
+          <button className="results-btn" onClick={() => handlePlayAgain()}>
+            Play Again
+          </button>
+        </div> : null}
     </>
   )
 };
